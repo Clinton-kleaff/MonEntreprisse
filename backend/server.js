02e2103f103e3758fa85a04e5ab312630aef0073
Clinton-kleaff <clintonpierre333@gmail.com>
2026-04-08 15:20:32 -0600

    cors and gitignore updates

--- backend/server.js
+++ backend/server.js
@@ -30,7 +30,7 @@ const allowedOrigins = [
   "http://localhost:5000",
   "http://127.0.0.1:5173",
   "https://monentreprise.onrender.com",
-  "https://businesscontrol.onrender.com"
+  "https://techsphere-qmbx.onrender.com"
 ];
 
 // Middleware
